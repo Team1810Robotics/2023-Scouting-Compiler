8d2dd97fa3013598654ed1a43f1e345cf94dfe98
willijoh005 <willijoh005@usd232.org>
2023-03-01 12:07:25 -0600

    didn't like needing an external libary so i rewrote it to work with just the java standard lib

--- src/main/java/io/github/team1810robotics/compiler/Main.java
+++ src/main/java/io/github/team1810robotics/compiler/Main.java
@@ -1,5 +1,7 @@
 package io.github.team1810robotics.compiler;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,17 +12,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.opencsv.CSVReader;
-import com.opencsv.CSVWriter;
-import com.opencsv.exceptions.CsvValidationException;
-
 public class Main {
     private static final int FILE_DEPTH = 1;
     private static final String COMPILED_FILE_NAME = "compiled";
 
     private static List<String[]> data = new ArrayList<>();
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         if (args.length == 0) {
             System.err.println("Need to give a path to the folder of csv files\n" +
                     "run with:\n\tgradlew run --args=\"path/to/folder\"");
@@ -34,9 +32,7 @@ public class Main {
 
             // write from the data list to the compiled csv
             String outputFile = args[0] + "/" + COMPILED_FILE_NAME + ".csv";
-            try (CSVWriter csvWriter = new CSVWriter(new FileWriter(outputFile))) {
-                csvWriter.writeAll(data);
-            }
+            writeToFile(data, outputFile);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -59,23 +55,33 @@ public class Main {
      *
      * @param filePaths list of strings of paths to the csv files to compile
      */
-    public static void readFiles(List<String[]> dataDst, List<String> filePaths) throws IOException, CsvValidationException {
+    public static void readFiles(List<String[]> dataDst, List<String> filePaths) throws IOException {
         // iterate through the paths reading into the data 2d list
         for (var path : filePaths) {
             addDataToList(dataDst, path);
         }
     }
 
-    private static void addDataToList(List<String[]> dataDst, String pathToFile) throws IOException, CsvValidationException {
-        // make reader for current file
-        try (CSVReader reader = new CSVReader(new FileReader(pathToFile))) {
-            String values[];
+    private static void addDataToList(List<String[]> dataDst, String pathToFile) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(pathToFile))) {
+            String line;
 
             // read data of current file and add it to the data list
-            while ((values = reader.readNext()) != null)
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split(",");
                 // assumes all headers are the same and stops any being printed again
                 if (dataDst.isEmpty() || !values[0].equals(dataDst.get(0)[0]))
                     dataDst.add(values);
+            }
+        }
+    }
+
+    private static void writeToFile(List<String[]> data, String filePath) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            // write data to file
+            for (var row : data) {
+                writer.write(String.join(",", row) + "\n");
+            }
         }
     }
 }
